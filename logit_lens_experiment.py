8d3c3ede486ed643f4acd9d6ba7992c8b303cd7c
YanaySoker <115056071+YanaySoker@users.noreply.github.com>
2024-05-20 23:37:00 +0300

    Update logit_lens_experiment.py

--- logit_lens_experiment.py
+++ logit_lens_experiment.py
@@ -69,39 +69,20 @@ for neighborhood in d[start_line:end_line]:
   for subject in subjects: 
     text = combine_prompt(subject, relation)
     encoded_input = tokenizer(text, return_tensors='pt').to('cuda')
-    output = model(**encoded_input, return_dict=True, output_hidden_states = True, output_hidden_states_mlp = True)
+    output = model(**encoded_input, return_dict=True, output_hidden_states = True)
 
-    hidden_states_mlp = output.hidden_states
-#     hidden_states = output.hidden_states
+    hidden_states = output.hidden_states
     labels_list = []
     print("subject:", subject)
     
-    # For FLS:
-    
-#     for l in range(len(hidden_states)):
-#       logits = model.lm_head(hidden_states[l])
-#       probs = torch.softmax(logits[:, -1], dim=1)
-#       labels = k_most_likely_one_layer(probs[0], K)
-#       labels_list.append(labels)    
-    
-    # For probs:
-    
-    labels_of_all_layers = []
-    max_probs_of_all_layers = []
-    for l in range(len(hidden_states_mlp)):   # new: mlp
-        logits = model.lm_head(hidden_states_mlp[l])   # new: mlp
-        probs = torch.softmax(logits[:, -1], dim=1)
-        labels, max_probs = k_most_likely_one_layer(probs[0], K, return_probs=True)
-        
-        labels_of_all_layers.append(labels)
-        max_probs_of_all_layers.append(max_probs)
+    for l in range(len(hidden_states)):
+      logits = model.lm_head(hidden_states[l])
+      probs = torch.softmax(logits[:, -1], dim=1)
+      labels = k_most_likely_one_layer(probs[0], K)
+      labels_list.append(labels)    
 
-    res_dict[relation][subject] = (labels_of_all_layers, max_probs_of_all_layers)
+    res_dict[relation][subject] = labels_list
   
   print("DONE relation:", relation)
 
-# For FLS:
-# print_dict(res_dict, f"labels_dict_gpt_j_{start_line}_{end_line}.py")
-
-print_dict(res_dict, f"probs_in_last_layer_{start_line}_{end_line}.py")
-# torch.save(res_dict, f"probs_dict_{start_line}_{end_line}.pt")
+print_dict(res_dict, f"labels_dict_{start_line}_{end_line}.py")
