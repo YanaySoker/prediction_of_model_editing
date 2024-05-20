29f986b1626b89ec9fc25e1da1c6dddedfb43f50
YanaySoker <115056071+YanaySoker@users.noreply.github.com>
2024-05-20 23:43:56 +0300

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,7 @@
 import torch
 from util import nethook
 import gc
-
+import math
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from util.generate import generate_interactive, generate_fast
@@ -13,20 +13,9 @@ import torch, numpy
 from collections import defaultdict
 from util import nethook
 from util.globals import DATA_DIR
-from experiments.causal_trace import (
-    ModelAndTokenizer,
-    layername,
-    guess_subject,
-    plot_trace_heatmap,
-)
-from experiments.causal_trace import (
-    make_inputs,
-    decode_tokens,
-    find_token_range,
-    # predict_token,
-    predict_from_input,
-    collect_embedding_std,
-)
+
+from experiments.causal_trace import predict_from_input,
+
 from dsets import KnownsDataset
 from config import *
 
@@ -81,7 +70,7 @@ def stack_matrices(A, B):
         row[j].append("*")
 
 
-def predict_from_input_with_probs(model, inp):    # yanay; based on origin "predict_from_input"
+def predict_from_input_with_probs(model, inp):    # based on origin "predict_from_input"
     out = model(**inp)["logits"]
     probs = torch.softmax(out[:, -1], dim=1)   
     p, preds = torch.max(probs, dim=1)        
@@ -89,10 +78,11 @@ def predict_from_input_with_probs(model, inp):    # yanay; based on origin "pred
 
 
 ## predict_token
-def predict_all_from_input(model, inp):    # yanay
+def predict_all_from_input(model, inp):    # based on origin "predict_from_input"
     out = model(**inp)["logits"]
     probs = torch.softmax(out[:, -1], dim=1)
     return probs
+
 
 def predict_token(mt, prompts, return_p=False, return_idx = False):
     inp = make_inputs(mt.tokenizer, prompts)
@@ -105,6 +95,7 @@ def predict_token(mt, prompts, return_p=False, return_idx = False):
         result = (result, preds)
 
     return result
+
 
 def predict_by_idx(mt, prompt, idx):     # yanay
   # model, str, int --> float
@@ -175,223 +166,6 @@ def print_list(list_input, file_name=None):
     file.close()
 
 
-def subset(sorted_list):
-  # count_list.sort()
-  S = sum(sorted_list)
-  start, end = 0, 0
-  while sum(sorted_list[start:end])<S/2:
-    end+=1
-  while sum(sorted_list[start:end])>S/2:
-    start+=1
-  if start==end:
-    return range(end-1)
-  return range(start,end)
-
-
-import math
-
-def trace_with_patch(
-    model,  # The model
-    inp,  # A set of inputs
-    states_to_patch,  # A list of (token index, layername) triples to restore
-    answers_t,  # Answer probabilities to collect
-    tokens_to_mix,  # Range of tokens to corrupt (begin, end)
-    noise,  # Level of noise to add
-    trace_layers=None,  # List of traced outputs to return
-):
-    prng = numpy.random.RandomState()  ### For reproducibility, use pseudorandom noise
-    patch_spec = defaultdict(list)
-    for t, l in states_to_patch:
-        patch_spec[l].append(t)
-    embed_layername = layername(model, 0, "embed")
-
-    def untuple(x):
-        return x[0] if isinstance(x, tuple) else x
-
-    # Define the model-patching rule.
-    def patch_rep(x, layer):
-        if layer == embed_layername:
-            # If requested, we corrupt a range of token embeddings on batch items x[1:]
-            if tokens_to_mix is not None:
-                b, e = tokens_to_mix
-                x[1:, b:e] += noise * torch.from_numpy(
-                    prng.randn(x.shape[0] - 1, e - b, x.shape[2])
-                ).to(x.device)
-            return x
-        if layer not in patch_spec:
-            return x
-        # If this layer is in the patch_spec, restore the uncorrupted hidden state
-        # for selected tokens.
-        h = untuple(x)
-        for t in patch_spec[layer]:
-            h[1:, t] = h[0, t]
-        return x
-
-    # With the patching rules defined, run the patched model in inference.
-    additional_layers = [] if trace_layers is None else trace_layers
-    with torch.no_grad(), nethook.TraceDict(
-        model,
-        [embed_layername] + list(patch_spec.keys()) + additional_layers,
-        edit_output=patch_rep,
-    ) as td:
-        outputs_exp = model(**inp)
-
-    # We report softmax probabilities for the answers_t token predictions of interest.
-    probs = torch.softmax(outputs_exp.logits[1:, -1, :], dim=1).mean(dim=0)[answers_t]
-
-    # If tracing all layers, collect all activations together to return.
-    if trace_layers is not None:
-        all_traced = torch.stack(
-            [untuple(td[layer].output).detach().cpu() for layer in trace_layers], dim=2
-        )
-        return probs, all_traced
-
-    return probs
-
-def trace_important_window(
-    model, num_layers, inp, e_range, answer_t, kind, window=10, noise=0.1
-):
-    ntoks = inp["input_ids"].shape[1]
-    table = []
-    for tnum in range(ntoks):
-        row = []
-        for layer in range(0, num_layers):
-            layerlist = [
-                (tnum, layername(model, L, kind))
-                for L in range(
-                    max(0, layer - window // 2), min(num_layers, layer - (-window // 2))
-                )
-            ]
-            r = trace_with_patch(
-                model, inp, layerlist, answer_t, tokens_to_mix=e_range, noise=noise
-            )
-            row.append(r)
-        table.append(torch.stack(row))
-    return torch.stack(table)
-
-def trace_important_states(model, num_layers, inp, e_range, answer_t, noise=0.1):
-    ntoks = inp["input_ids"].shape[1]
-    table = []
-    for tnum in range(ntoks):
-        row = []
-        for layer in range(0, num_layers):
-            r = trace_with_patch(
-                model,
-                inp,
-                [(tnum, layername(model, layer))],
-                answer_t,
-                tokens_to_mix=e_range,
-                noise=noise,
-            )
-            row.append(r)
-        table.append(torch.stack(row))
-    return torch.stack(table)
-
-def calculate_hidden_flow(
-    mt, prompt, subject, samples=10, noise=0.1, window=10, kind=None
-):
-    """
-    Runs causal tracing over every token/layer combination in the network
-    and returns a dictionary numerically summarizing the results.
-    """
-    inp = make_inputs(mt.tokenizer, [prompt] * (samples + 1))
-    with torch.no_grad():
-        answer_t, base_score = [d[0] for d in predict_from_input(mt.model, inp)]
-    [answer] = decode_tokens(mt.tokenizer, [answer_t])
-    e_range = find_token_range(mt.tokenizer, inp["input_ids"][0], subject)
-    low_score = trace_with_patch(
-        mt.model, inp, [], answer_t, e_range, noise=noise
-    ).item()
-    if not kind:
-        differences = trace_important_states(
-            mt.model, mt.num_layers, inp, e_range, answer_t, noise=noise
-        )
-    else:
-        differences = trace_important_window(
-            mt.model,
-            mt.num_layers,
-            inp,
-            e_range,
-            answer_t,
-            noise=noise,
-            window=window,
-            kind=kind,
-        )
-    differences = differences.detach().cpu()
-    return dict(
-        scores=differences,
-        low_score=low_score,
-        high_score=base_score,
-        input_ids=inp["input_ids"][0],
-        input_tokens=decode_tokens(mt.tokenizer, inp["input_ids"][0]),
-        subject_range=e_range,
-        answer=answer,
-        window=window,
-        kind=kind or "",
-    )
-
-def return_map(
-    prompt,
-    subject,
-    mt=mt,
-    noise=noise_level,
-    samples=10,
-    window=10,
-    kind="mlp",
-    modelname=None,
-    savepdf=None,
-):
-    if subject is None:
-        subject = guess_subject(prompt)
-    result = calculate_hidden_flow(
-        mt, prompt, subject, samples=samples, noise=noise, window=window, kind=kind
-    )
-    return result
-
-
-def generate_city_prompt(city):
-  prompt = "is the capital city of"
-  word=naiv_predict(city, prompt)
-  while word in ["the", "state", "State", "of", "Republic", "province", "Province"]:
-    prompt = prompt+ " " + word
-    word=naiv_predict(city, prompt)
-  return prompt
-
-
-def entropy(tens):
-    tens_norm = tens / tens.sum()
-    logs = torch.log2(tens_norm)
-    logs = torch.where(logs==-float("inf"),0,logs)
-    y = logs * tens_norm
-    return -y.sum().item() / math.log2(len(tens))
-
-
-def max_layer_and_entropy(prompt, subject, max_neighbors=[1], effect_idx=[]):
-  result = return_map(prompt, subject)
-  scores = result['scores']
-  a, b = result['subject_range']
-  argmax = scores[a:b].argmax().item()
-
-  relevant_token_idx = int(argmax / len(scores[0])) + a
-  relevant_token = scores[relevant_token_idx]
-
-  _max = scores[a:b].max().item()
-  _min = scores[a:b].min().item()
-  avrg = relevant_token.sum().item() / (len(scores[0]))
-  effs = []
-  for idx in effect_idx: 
-    effs.append(relevant_token[idx].item())
-
-  layer = argmax % len(scores[0])
-  cent = []
-  for i in max_neighbors:
-    if layer+i>=0:
-      cent.append(((relevant_token[layer] - relevant_token[layer+1]) / relevant_token[layer]).item())
-    else:
-      cent.append(-1)
-  return layer, entropy(relevant_token), cent, _max, _min, avrg, effs
-
-
 def clean(mt):
     print(clean)
     if "orig_weights" in M.keys():
@@ -406,6 +180,7 @@ def clean(mt):
     gc.collect()
     if M is not None:
         M.clear()
+
 
 def encode(token):
   if token[0]!=" ":
