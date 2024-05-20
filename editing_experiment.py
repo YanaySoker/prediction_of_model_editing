65650eb085117be768b8e63f69f3e5271c92f9ee
YanaySoker <115056071+YanaySoker@users.noreply.github.com>
2024-05-21 02:02:58 +0300

    Update editing_experiment.py

--- editing_experiment.py
+++ editing_experiment.py
@@ -7,5 +7,5 @@ import sys
 start_line, end_line = int(sys.argv[1]), int(sys.argv[2])
 print("lines:", start_line, "to", end_line, "; experiment type: high res")
 results = all_results(d[start_line:end_line], HIGH_RES_FLAG = True)
-file_name=f"attractions_and_features_high_res_{start_line}_{end_line}.py"
+file_name=f"scores_{start_line}_{end_line}.py"
 print_list(results, file_name)
