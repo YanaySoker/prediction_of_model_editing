34482b2b815aeb542f52124352d226366d4b8318
YanaySoker <115056071+YanaySoker@users.noreply.github.com>
2024-05-21 21:00:22 +0300

    Add files via upload

---
+++ run.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -N 1 # number of minimum nodes
+#SBATCH -c 2 # number of cores
+#SBATCH --gres=gpu:1 # Request 1 gpu
+#SBATCH -o slurm.%N.%j.out # stdout goes here
+#SBATCH -e slurm.%N.%j.out # stderr goes here
+
+# sbatch -p nlp -A nlp -w nlp-a40-1 --gres=gpu:1 run.bash 
+
+
+
+nvidia-smi
+CONDA_HOME=$home/miniconda3
+CONDA_ENV=spcf_of_rome
+CUDA_LAUNCH_BLOCKING=1 
+python /home/yanay.soker/Specificity_of_ROME/rome/update_experiment.py  "$1" "$2" "$3"
