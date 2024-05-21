60124c780aec753cf160891cf678152df7d1159b
YanaySoker <115056071+YanaySoker@users.noreply.github.com>
2024-05-21 21:04:30 +0300

    Update and rename run_ll.bash to run_logit_lens.bash

--- run_logit_lens.bash
+++ run_logit_lens.bash
@@ -5,13 +5,10 @@
 #SBATCH -o slurm.%N.%j.out # stdout goes here
 #SBATCH -e slurm.%N.%j.out # stderr goes here
 
-# sbatch -p nlp -A nlp -w nlp-a40-1 --gres=gpu:1 run_ll.bash 
-
-
 nvidia-smi
 CONDA_HOME=$home/miniconda3
-CONDA_ENV=spcf_of_rome
+CONDA_ENV=prediction_editing_env
 CUDA_LAUNCH_BLOCKING=1 
-python /home/yanay.soker/Specificity_of_ROME/rome/logit_lens_experiment.py "$1" "$2"
+python /home/prediction_of_model_editing/logit_lens_experiment.py "$1" "$2"
 
 
